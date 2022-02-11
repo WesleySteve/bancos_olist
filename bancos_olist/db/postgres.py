457f7aa37a454479024ddd2d662fecc665af6ecd
Wesley Steve <wesley.silva23@hotmail.com>
2022-02-11 16:58:39 -0300

    feat: definido a funçao connect_db_postgres

--- bancos_olist/db/postgres.py
+++ bancos_olist/db/postgres.py
@@ -1,2 +1,17 @@
 import os
 
+import dotenv
+import sqlalchemy
+
+
+def connect_db_postgres(dotenv_path=os.path.expanduser("~/.env")):
+    """Função para se conectar no banco de dados
+
+    Args:
+        dotenv_path (caminho das variaveis de ambiente, optional):
+        pesquisa .env iniciando na raiz do usuario caso ele não passe um caminho.
+        caminho padrão: os.path.expanduser("~/.env").
+
+    Returns:
+        conexao: retorna a conexao com o banco de dados postgres
+    """
